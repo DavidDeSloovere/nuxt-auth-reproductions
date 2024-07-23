c95216abf3ac6031c21cec467feaedaccf540b4b
David De Sloovere <david.desloovere@outlook.com>
2024-07-23 14:50:51 +0200

    chore: hard code base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
   runtimeConfig: {
     authSecret: '123',
   },
+  auth: {
+    baseURL: 'https://nuxt-auth-reproductions.vercel.app/api/auth',
+  },
   typescript: {
     shim: false
   }
