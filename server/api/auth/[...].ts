222a36ad45365422ef35f7f66e16ea39acfdca76
David De Sloovere <david.desloovere@outlook.com>
2024-07-23 14:55:23 +0200

    chore: cleanup

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,11 +1,8 @@
 import CredentialsProvider from 'next-auth/providers/credentials'
-import GithubProvider from 'next-auth/providers/github'
 import { NuxtAuthHandler } from '#auth'
 
 export default NuxtAuthHandler({
-  // TODO: SET A STRONG SECRET, SEE https://sidebase.io/nuxt-auth/configuration/nuxt-auth-handler#secret
   secret: useRuntimeConfig().authSecret,
-  // TODO: ADD YOUR OWN AUTHENTICATION PROVIDER HERE, READ THE DOCS FOR MORE: https://sidebase.io/nuxt-auth
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     CredentialsProvider.default({
